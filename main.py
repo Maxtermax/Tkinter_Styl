003cb3b929417af86d5854a1291d1972fcf82e15
maxtermax <esneyder_a@hotmail.com>
2014-09-23 16:34:37 -0500

    Styles with json

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ Test=fs.readlines()
 fs.close()
 ctx=Tk()
 
-
-class algo:
-	def __init__(self,ctx):
-		self.context=ctx
-
-	def crear(self,key,val):
-		self.titulo=Label(self.context)
-		self.titulo["text"]="fewfge"
-		self.titulo[key]=val
-		self.titulo.pack()
-	def run(self):
-		self.context.mainloop()
-
-ins=algo(ctx)
-
 json={
 	"bg":"red",
 	"fg":"white",
