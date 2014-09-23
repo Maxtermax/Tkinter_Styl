fab75661c476c0570a010909c7354d207404bf58
maxtermax <esneyder_a@hotmail.com>
2014-09-23 16:34:02 -0500

    Styles with json

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
-import subprocess as comandos
-import sys 
+from pymongo import MongoClient
 from Tkinter import *
+import subprocess as comandos
+from gridfs import GridFS
+import sys 
 import re 
 
-
+fs = open("Style.txt","r")#read , write 
+Test=fs.readlines()
+fs.close()
 ctx=Tk()
 
 
-'''
-lista=[2,"Indexame",5,50,5]
-lista.pop()#saca el ultimo elemento 
-lista.pop(1)#saca el elemento de la lista mediante el indice
-lista.remove(1)#saca el elemento de la lista mediante el nombre 
+class algo:
+	def __init__(self,ctx):
+		self.context=ctx
+
+	def crear(self,key,val):
+		self.titulo=Label(self.context)
+		self.titulo["text"]="fewfge"
+		self.titulo[key]=val
+		self.titulo.pack()
+	def run(self):
+		self.context.mainloop()
+
+ins=algo(ctx)
+
+json={
+	"bg":"red",
+	"fg":"white",
+	"text":"Esneyder es la leche"
+}
+
+Esneyder=Label(ctx,json)
+
+Esneyder.pack()
+Esneyder.mainloop()
 
 
-lista[0:2]#devuelve un slice desde 0 hasta el index q queramos +1
-
-lista.index("Indexame")#devuelve el indice
-lista.insert(0,"Agrega esto despues del indice 0")
-lista.count(5)#devuelve cuantas veces se repeti el elemento
-
-lista.sort(reverse=True)
-
-def potenciador(n):
-	return n*n
-print map(potenciador,[0,34,5])
-'''
-
-fs = open("Style.txt","r")
-Test=fs.readlines()
-fs.close()
 
 
 
-def Styl():
-	Id=""
-	for i in Test:
-		if(re.search(":$",i)):
-			#find the id 
-			Id=i
-		elif("color" in i):
-			attribute=re.split(" ",i)[1].replace("\n","")
-			print attribute #value of the atribute 
-			#Add attribute to Tkinter Here.
-		elif("background" in i):
-			attribute=re.split(" ",i)[1].replace("\n","")
-			print attribute #value of the atribute 
-			#Add attribute to Tkinter Here.
-		
-		elif("padding" in i):
-			attribute=re.split(" ",i)[1].replace("\n","")
-			print attribute #value of the atribute 
-			#Add attribute to Tkinter Here.
 
-print Styl()#Como parametro resive el TKinter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
