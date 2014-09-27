7381f6475538ad044cd39c637454cb73df0d54fa
maxtermax <esneyder_a@hotmail.com>
2014-09-26 22:03:12 -0500

    Clase formulario y eventos agregados

--- main.py
+++ main.py
@@ -15,55 +15,91 @@ root.geometry("654x350+250+150")
 root.maxsize(width=654,height=350)
 root.minsize(width=654,height=350)
 
-Title = Label(root,data["Title"])
-Title.place(data["Title_pos"])
-
-expU = '[a-z]+-?[a-z]+'
-expP = '/(^(?=.*[a-z])(?=.*[A-Z])(?=.*\d){6,20}.+$)/'
-
-def vli():
-	Uname = entry.get()
-	initU = str(db.Colecion.find_one({"name":Uname}))
-	initP = str(db.Colecion.find_one({"pass":Uname}))
-	if re.findall(expU,Uname) == [init:]:
-		if re.findall(expP,EPass) == [initP]:
-			print "Bienvenido"
-	else:
-		print "Error"
-
-Uname = Label(root,data["Uname"])
-Uname.place(data["Uname_pos"])
 
 
+class Form:
+	def __init__(self,root):
+		self.root = root #Guarda la instancia principal de TK() dentro de self.root para que pueda se usada en otras funciones
 
-Ename = Entry(root,data["Ename"])
-Ename.place(data["Ename_pos"])
+	def Input(self,style,pos,cb):
+		self.Input_name=Entry(self.root,style)
+		self.Input_name.place(pos)
+		return cb(self.Input_name) #cb=callback
 
+		#Retorna el mismo elemento que se crea en la instancia por medio del callback de la funcion que lo maneja
+		#Para que se pueda definir una funcion que va a manejar todo lo que tenga que ver con ese elemento			 
 
-PassU = Label(root,data["PassU"])
-PassU.place(data["PassU_pos"])
-
-EPass = Entry(root,data["EPass"])
-EPass.place(data["EPass_pos"])
-
-
-Entrar = Button(root,data["entrar"], command= validar)
-Entrar.place(data["entrar_pos"])
-Entrar.config(cursor="spraycan")
-
-
-'''
-photo = PhotoImage(file="Bat.gif")
-w = Label(root, image=photo,width=280,height=245)
-w.photo = photo
-w.place(x=350,y=40)
-'''
-
-root.title("Iniciar sesion")
+	def Button(self,style,pos,cb):
+		self.Button_name = Button(self.root,style)
+		self.Button_name.place(pos) #Ubica el elemento segun lo que llegue por parametros en el eje x,y
+		return cb(self.Button_name)
+		#Retorna el mismo elemento que se crea en la instancia por medio del callback de la funcion que lo maneja
+		#Para que se pueda definir una funcion que va a manejar todo lo que tenga que ver con ese elemento			 
 
 
-root.config(bg="#fff")
-root.mainloop()
+	def Label(self,style,pos):
+		self.Label_name = Label(root,style)
+		self.Label_name.place(pos)
+
+
+	def Run(self,style):
+		self.root.config(style)
+		self.root.mainloop()
+
+
+
+form = Form(root) #Instancia de la clase formulario 
+
+form.Label(data["Title"],data["Title_pos"])
+form.Label(data["Uname"],data["Uname_pos"])
+form.Label(data["PassU"],data["PassU_pos"])
+
+
+
+
+def Submit_Handler(btn):
+	def Over_Submit(event):
+		print 'Sobre Enviar'
+		btn.config({"bg":"red"})
+
+	def Out_Submit(event):
+		print 'Fuera de Enviar'
+		btn.config({"bg":"#016899"})
+
+	btn.bind("<Enter>",Over_Submit) 
+	btn.bind("<Leave>",Out_Submit)	
+	btn.bind("<FocusIn>",Over_Submit)
+	btn.bind("<FocusOut>",Out_Submit)	
+
+
+
+
+
+def Pass_Handler(input):
+	def Over_Pass(event):
+		input.config({"bg":"red"})
+
+	def Out_Pass(event):
+		input.config({"bg":"#fff"})
+
+	input.bind("<Enter>",Over_Pass)
+	input.bind("<Leave>",Out_Pass)
+	input.bind("<FocusIn>",Over_Pass)
+	input.bind("<FocusOut>",Out_Pass)	
+
+
+def Name_Handler(input):
+	print "Otra funcion que maneja el nombre"
+
+
+
+form.Input(data["EPass"],data["EPass_pos"],Pass_Handler)
+form.Input(data["Ename"],data["Ename_pos"],Name_Handler)
+form.Button(data["entrar"],data["entrar_pos"],Submit_Handler)
+form.Run({"bg":"white"})
+
+
+
 
 
 
